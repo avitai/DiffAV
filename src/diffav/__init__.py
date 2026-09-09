2bb0ea2f5b878be1dbaf952e3d415e72e495b28d
Mahdi Shafiei <m.mahdi.shafiei@gmail.com>
2026-09-09 16:52:28 -0700

    chore(release): 0.1.1

--- src/diffav/__init__.py
+++ src/diffav/__init__.py
@@ -5,4 +5,4 @@ scenarios to evaluate the safety and performance of autonomous vehicle stacks.
 Built on the JAX ecosystem with datarax, artifex, opifex, and calibrax.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
